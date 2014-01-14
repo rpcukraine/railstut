c10071c04c623605697c92af7a05744862faea44
Yurand <polyukhovich@gmail.com>
2014-01-14 12:02:50 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railstut::Application.config.secret_key_base = '9c88c15acc7dd3af4cbc64e77028add8cac5282de3927904f36538775408a6190e9e5aa166906bedeab6ceaf617f808a8e7aad6bf0cae7572a48e0a3658a80bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railstut::Application.config.session_store :cookie_store, key: '_railstut_session'
